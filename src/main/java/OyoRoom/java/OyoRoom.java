46439e271a82001b26600a31a8c99673aeeae827
Syeda Saniya Mehdi <saniya2830@gmail.com>
2022-10-14 10:14:26 +0530

    Initial commit

---
+++ src/main/java/OyoRoom/java/OyoRoom.java
@@ -0,0 +1,18 @@
+package OyoRoom.java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OyoRoom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver=new EdgeDriver();
+		driver.get("https://www.oyorooms.com/");
+
+	}
+
+}
